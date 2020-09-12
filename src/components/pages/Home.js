6bf5beb7926143da7f1b36d33879c9c020e542e5
eltoncampos1 <qazwsx272518>
2020-09-12 17:15:47 -0300

    Project completed

---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer';
+
+import Cards from '../Cards';
+import HeroSection from '../HeroSection';
+
+function Home () {
+    return (
+        <>
+            <HeroSection />
+            <Cards />
+            <Footer />
+        </>
+    )
+}
+export default Home;
